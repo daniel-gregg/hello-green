b0b192436472905f14bd8d2ccb422b1aff7d4655
macoovacany <macoovacany@hotmail.com>
2021-06-10 20:31:03 +0930

    single user in databse

--- models/index.js
+++ models/index.js
@@ -1,19 +1,23 @@
 const User = require('./user');
-const Brief = require('./brief');
-const Organisation = require('./organisation');
-const Image = require('./image');
-const Keyword = require('./keyword');
-const OrganisationUser = require('./organisationUser');
-const BriefKeyword = require('./briefKeyword');
-const UserBrief = require('./userBrief');
+// const Brief = require('./brief');
+// const Organisation = require('./organisation');
+// const Image = require('./image');
+// const Keyword = require('./keyword');
+// const OrganisationUser = require('./organisationUser');
+// const BriefKeyword = require('./briefKeyword');
+// const UserBrief = require('./userBrief');
+
+// User.hasMany(Brief);
+// Brief.belongsTo(User, { foreignKey: 'owner_id' });
 
 module.exports = {
     User,
-    Image,
-    Organisation,
-    Brief,
-    Keyword,
-    OrganisationUser,
-    BriefKeyword,
-    UserBrief,
+
+    // Image,
+    // Organisation,
+    // Brief,
+    // Keyword,
+    // OrganisationUser,
+    // BriefKeyword,
+    // UserBrief,
 };
