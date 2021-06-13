23b8913c7bfdadfc86e30d6c5d19767a71f5f465
macoovacany <macoovacany@hotmail.com>
2021-06-13 19:09:10 +0930

    Seeded Organisation User Relationship

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,14 @@
 const sequelize = require('../config/connection');
 // const { User, Brief, Keyword, Organisation } = require('../models');
 
-const { User, Organisation, Brief, Keyword } = require('../models');
+const { User, Organisation, Brief, Keyword, OrganisationUser } = require('../models');
 
 const userData = require('./data/userData.json');
 const briefData = require('./data/briefData.json');
 const organisationData = require('./data/organisationData.json');
 const keywordData = require('./data/keywordData.json');
+
+const organisationUserData = require('./data/organisationUserData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -15,11 +17,16 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
-    
+
     const organisations = await Organisation.bulkCreate(organisationData);
     const keywords = await Keyword.bulkCreate(keywordData);
     const briefs = await Brief.bulkCreate(briefData);
-    
+
+    const userOrg = await OrganisationUser.bulkCreate(organisationUserData, {
+        individualHooks: true,
+        returning: true,
+    });
+
     process.exit(0);
 };
 
