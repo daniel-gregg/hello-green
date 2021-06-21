ebba35d63374d8fff76d3c0c927dca7bd37e36bf
Spaniel Boone <spaniel.boone@gmail.com>
2021-06-21 20:32:04 +0930

    Bio update working

--- public/js/bioupdate.js
+++ public/js/bioupdate.js
@@ -7,16 +7,24 @@ const bioFormHandler = async (event) => {
     const last_name = document.querySelector('#last_name').value;
     const email = document.querySelector('#email').value;
     const organisation = document.querySelector('#organisation').value;
-    const bio = document.querySelector('#bio').textContent;
+    const text = document.querySelector('#bio').value;
+
+    console.log(document.querySelector('#userId').value);
 
     const response = await fetch(`/bio/edit`, {
         method: 'PUT',
-        body: JSON.stringify({ first_name, last_name, email, organisation, bio }),
+        body: JSON.stringify({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            organisation: organisation,
+            text: text,
+        }),
         headers: {
             'Content-Type': 'application/json',
         },
     });
-
+    console.log(response);
     if (response.ok) {
         document.location.replace('/dashboard');
     } else {
